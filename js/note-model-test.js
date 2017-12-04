533c5e7a4ab2b4361547db1be2932ac039457e0e
majdeddine <majda-jambouri@hotmail.fr>
2017-12-04 16:17:30 +0000

    add html file and pass the test

--- js/note-model-test.js
+++ js/note-model-test.js
@@ -1,7 +1,7 @@
 (function(exports){
   function test(){
     var note = new Note('My favourite language is JavaScript')
-    assert.isTrue(note.text === 'My favourite language is JavaScript' )
+    assert.isTrue(note.text() === 'My favourite language is JavaScript' )
   }
   test();
 })(this)
--- js/note-model.js
+++ js/note-model.js
@@ -1,6 +1,9 @@
 (function(exports){
   function Note(text){
-    this.text = text;
+    this._text = text;
+  }
+  Note.prototype.text = function(){
+    return this._text;
   }
   exports.Note = Note;
 })(this)
