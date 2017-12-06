95a092e2dafdb035394e727a44faff29aeddad40
majdeddine <majda-jambouri@hotmail.fr>
2017-12-06 20:45:54 +0000

    add a unique id for each note model object

--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -1,5 +1,17 @@
+var note = new Note('My favourite language is JavaScript')
+
 function testNoteTextReturnPassedArgument(){
-  var note = new Note('My favourite language is JavaScript')
-  assert.isTrue(note.text() === 'My favourite language is JavaScript' )
+  assert.isTrue(note.text() === 'My favourite language is JavaScript')
 }
 testNoteTextReturnPassedArgument();
+
+function testNoteInstanceHaveId(){
+  assert.isTrue(note.id()=== 0)
+}
+testNoteInstanceHaveId()
+
+function testNoteInstanceHaveUniqueId(){
+  var note1 = new Note();
+  assert.isTrue(note1.id()=== 1)
+}
+testNoteInstanceHaveUniqueId();
--- src/note-model.js
+++ src/note-model.js
@@ -1,9 +1,17 @@
 (function(exports){
+  var i = -1;
   function Note(text=''){
+    var id = (function(){i++; return i})()
     this._text = text;
+    this._id = id ;
+
   }
   Note.prototype.text = function(){
     return this._text;
+  }
+
+  Note.prototype.id = function(){
+    return this._id;
   }
   exports.Note = Note;
 })(this)
