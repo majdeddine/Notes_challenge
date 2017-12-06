e6dedd7c611add2a34b96f102cf7e5a73b2891a9
majdeddine <majda-jambouri@hotmail.fr>
2017-12-06 12:40:28 +0000

    mock the note view in the note controller to print a list

--- js/note-controller-test.js
+++ js/note-controller-test.js
@@ -1,3 +1,5 @@
+var doubleViewNote ={toHtml: "<ul><li><div>Favourite drink: seltzer</div></li></ul>" };
+
 function returnHtmlElement (){
   var noteController = new NoteController();
   noteController.getElementOf('app');
@@ -13,3 +15,11 @@ function changeHtmlElement(){
   assert.isTrue(noteController._element.innerHTML === 'ola')
 }
 changeHtmlElement();
+
+function changeHtmlElementToList(){
+  var noteController = new NoteController();
+  noteController.getElementOf('app');
+  noteController.changeContent(doubleViewNote.toHtml);
+  assert.isTrue(noteController._element.innerHTML === "<ul><li><div>Favourite drink: seltzer</div></li></ul>")
+}
+changeHtmlElementToList();
