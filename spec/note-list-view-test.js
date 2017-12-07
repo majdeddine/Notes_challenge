d06c0e862879fe8a49d61adafc151f6bf7d5a13e
majdeddine <majda-jambouri@hotmail.fr>
2017-12-07 00:13:31 +0000

    link each note to a url

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,7 +1,9 @@
-var doubleNoteList ={list:function(){return []}}
+
+var doubleNote ={text: function(){ return ""}}
+var doubleNoteList ={list: function(){var arr = [doubleNote]; return arr}}
+var noteListView = new NoteListView(doubleNoteList);
 
 function returnAStringOfHtml(){
-  var noteListView = new NoteListView(doubleNoteList);
   noteListView.minifyText();
   assert.isTrue(noteListView.toHtml() === "<ul><li><div></div></li></ul>")
 }
@@ -28,3 +30,11 @@ function returnAListThatHasSeveralNotes(){
   assert.isTrue(noteListView.toHtml() === "<ul><li><div>Favourite food: pest</div></li><li><div>Favourite drink: sel</div></li></ul>")
 }
 returnAListThatHasSeveralNotes();
+
+function returnAListOfNoteWithUrl(){
+  noteListView.minifyText();
+  noteListView.addUrl();
+  console.log(noteListView.toHtml())
+  assert.isTrue(noteListView.toHtml() === "<ul><li><div><a href=\"#note/1\"></a></div></li></ul>");
+}
+returnAListOfNoteWithUrl();
